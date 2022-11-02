7bfbd7dab97d6e22b98ed419736ac3e73cae1f6e
Xarderos <99949891+Xarderos@users.noreply.github.com>
2022-11-02 19:15:05 +0100

    Delete Front Palm Tree Top 01.tsx

--- citm_desvj_project_template-L08/Output/Assets/Maps/Front Palm Tree Top 01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="Front Palm Tree Top 01" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../Textures/Front Palm Tree Top 01.png" width="39" height="32"/>
-</tileset>
