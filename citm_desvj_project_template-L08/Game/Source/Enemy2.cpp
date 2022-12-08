75b8688cf11223ce77b372197147d79e3cd299df
GHOSTVCL <joanetgiolet@gmail.com>
2022-12-08 18:18:24 +0100

    seashell feta

--- citm_desvj_project_template-L08/Game/Source/Enemy2.cpp
+++ citm_desvj_project_template-L08/Game/Source/Enemy2.cpp
@@ -12,6 +12,17 @@
 Enemy2::Enemy2() : Entity(EntityType::ENEMY2)
 {
 	
+	name.Create("Enemy2");
+
+	seashellidle.PushBack({ 0,152,48,38 });
+	seashellidle.PushBack({ 48,152,48,38 });
+	seashellidle.PushBack({ 96,152,48,38 });
+	seashellidle.PushBack({ 144,152,48,38 });
+	seashellidle.PushBack({ 192,152,48,38 });
+	seashellidle.PushBack({ 240,152,48,38 });
+	seashellidle.loop = true;
+	seashellidle.speed = 0.1f;
+
 }
 
 Enemy2::~Enemy2() {}
@@ -24,12 +35,21 @@ bool Enemy2::Awake() {
 
 bool Enemy2::Start() {
 
+	texture = app->tex->Load("Assets/Textures/Spritesheets/Seashell.png");
+
+	ebody = app->physics->CreateRectangleSensor(position.x + 15, position.y + 24, 25, 20, bodyType::STATIC);
+	ebody->ctype = ColliderType::DEATH;
+	ebody->listener = this;
 
 	return true;
 }
 
 bool Enemy2::Update()
 {
+
+	seashell = seashellidle.GetCurrentFrame();
+	seashellidle.Update();
+	app->render->DrawTexture(texture, position.x - 12, position.y - 3, &seashell);
 
 	return true;
 }
